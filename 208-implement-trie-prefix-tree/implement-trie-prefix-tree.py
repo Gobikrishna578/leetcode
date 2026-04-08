5efcf3d61b1f640568403530ed93cc6e87a3a6aa
Gobikrishna578 <gobikrishna578@gmail.com>
2026-04-08 21:53:54 +0530

    Time: 85 ms (66.11%) | Memory: 39 MB (38.42%) - LeetSync

---
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.py
@@ -0,0 +1,29 @@
+class Trie:
+
+    def __init__(self):
+        self.children = {}
+        self.is_end = False
+
+    def insert(self, word):
+        node = self
+        for ch in word:
+            if ch not in node.children:
+                node.children[ch] = Trie()
+            node = node.children[ch]
+        node.is_end = True
+
+    def search(self, word):
+        node = self
+        for ch in word:
+            if ch not in node.children:
+                return False
+            node = node.children[ch]
+        return node.is_end
+
+    def startsWith(self, prefix):
+        node = self
+        for ch in prefix:
+            if ch not in node.children:
+                return False
+            node = node.children[ch]
+        return True
