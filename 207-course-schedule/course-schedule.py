6f1e87e69940cd541a28769a6b03367ab7a48f69
Gobikrishna578 <gobikrishna578@gmail.com>
2026-04-29 21:32:42 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (99.62%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -2,7 +2,7 @@ from collections import deque
 
 class Solution:
     def canFinish(self, numCourses, prerequisites):
-        # Step 1: graph + indegree
+        
         graph = [[] for _ in range(numCourses)]
         indegree = [0] * numCourses
         
@@ -10,13 +10,13 @@ class Solution:
             graph[b].append(a)
             indegree[a] += 1
         
-        # Step 2: queue for 0 indegree
+       
         q = deque()
         for i in range(numCourses):
             if indegree[i] == 0:
                 q.append(i)
         
-        # Step 3: process
+        
         count = 0
         while q:
             course = q.popleft()
@@ -27,5 +27,5 @@ class Solution:
                 if indegree[nei] == 0:
                     q.append(nei)
         
-        # Step 4: check
+       
         return count == numCourses
